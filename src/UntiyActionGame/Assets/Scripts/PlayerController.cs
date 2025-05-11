738ab50163d317185461fb29056b391810683325
YouZhengHua <42639936+YouZhengHua@users.noreply.github.com>
2025-05-11 12:26:39 +0800

    feat: 更新玩家控制器

--- src/UntiyActionGame/Assets/Scripts/PlayerController.cs
+++ src/UntiyActionGame/Assets/Scripts/PlayerController.cs
@@ -1,12 +1,26 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    private Vector2 movementInput;
+    private Vector2 _movementInput;
+    private Rigidbody2D _rb;
+    private Animator _characterAnimator;
+    private SpriteRenderer _characterSpriteRenderer;
+    
+    private readonly string _animatorMoveName = "Move";
 
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private InputActionReference moveInput;
+
+    private void Awake()
+    {
+        _rb = this.GetComponent<Rigidbody2D>();
+        _characterAnimator = this.GetComponentInChildren<Animator>();
+        _characterSpriteRenderer = this.GetComponentInChildren<SpriteRenderer>();
+    }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,14 +30,38 @@ public class PlayerController : MonoBehaviour
         moveInput.action.canceled += OnMove;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * moveSpeed * Time.deltaTime);
+        if (_movementInput.magnitude > 0f)
+        {
+            _rb.AddForce(_movementInput * moveSpeed, ForceMode2D.Impulse);
+            
+            // 設定速度上限
+            if (Mathf.Abs(_rb.linearVelocity.x) > moveSpeed)
+            {
+                _rb.linearVelocity = new Vector2((_rb.linearVelocity.x > 0f ? 1f : -1f) * moveSpeed, _rb.linearVelocity.y);
+            }
+        }
+        else
+        {
+            _rb.linearVelocity = new Vector2(0f, _rb.linearVelocity.y);
+        }
+
     }
+
     private void OnMove(InputAction.CallbackContext context)
     {
-        movementInput = context.ReadValue<Vector2>();
+        _movementInput = context.ReadValue<Vector2>();
+        _movementInput = new Vector2(_movementInput.x, 0f);
+        _characterAnimator.SetFloat(_animatorMoveName, Mathf.Abs(_movementInput.x));
+        if (_movementInput.x > 0f)
+        {
+            _characterSpriteRenderer.flipX = false;
+        }
+        else if (_movementInput.x < 0f)
+        {
+            _characterSpriteRenderer.flipX = true;
+        }
     }
 }
   
